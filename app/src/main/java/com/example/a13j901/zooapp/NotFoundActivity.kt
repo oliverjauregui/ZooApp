955631dfceda7cc66f513defd3a004a46efd590d
John Doe <oliver.jauregui20@gmail.com>
2018-12-01 11:47:49 -0500

    first commit

---
+++ app/src/main/java/com/example/a13j901/zooapp/NotFoundActivity.kt
@@ -0,0 +1,12 @@
+package com.example.a13j901.zooapp
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+
+class NotFoundActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_not_found)
+    }
+}
